fd8382e3c66bf1e73ff241bf1863e150f942e814
Rafael Trindade <rst2trindade@gmail.com>
2018-02-22 13:24:20 -0400

    initial commit

---
+++ app/src/main/java/farmaciaagora/alphalumos/com/appusuariofarmaciaagora/FazerPedidoActivity.java
@@ -0,0 +1,13 @@
+package farmaciaagora.alphalumos.com.appusuariofarmaciaagora;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FazerPedidoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fazer_pedido);
+    }
+}
